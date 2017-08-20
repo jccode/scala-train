10ba9a0f3ae9984ae1ad0062c73cd7ff6d8bb264
jessie <junchangchen@gmail.com>
2017-08-20 10:42:45 +0800

    device manager spec

--- src/test/scala/ch_akka/iot/DeviceManagerSpec.scala
+++ src/test/scala/ch_akka/iot/DeviceManagerSpec.scala
@@ -1,17 +1,19 @@
 package ch_akka.iot
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, PoisonPill}
 import akka.testkit.{TestActorRef, TestKit, TestProbe}
 import ch_akka.iot.Device.{RecordTemperature, TemperatureRecorded}
 import ch_akka.iot.DeviceManager.{DeviceRegistered, ReplyGroupList, RequestGroupList, RequestTrackDevice}
 import org.scalatest._
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class DeviceManagerSpec(_system: ActorSystem) extends TestKit(_system) with FlatSpecLike with Matchers with BeforeAndAfterAll {
 
   def this() = this(ActorSystem("DeviceManagerSpec"))
 
   override protected def afterAll(): Unit = shutdown(system)
-  
+
 
   it should "be able to register a device actor" in {
     val probe = TestProbe()
@@ -78,20 +80,21 @@ class DeviceManagerSpec(_system: ActorSystem) extends TestKit(_system) with Flat
     response.requestId should equal(0)
     response.ids should equal(Set("group1", "group2"))
 
-//    val groupPath = deviceActor1.path.parent
-//    val toShutdown = system.actorSelection(groupPath)
-//
-//    println(groupPath)
+    val groupPath = deviceActor1.path.parent
 
+    system.actorSelection(groupPath).resolveOne(5 seconds).map {
+      group1ActorRef => {
+        probe.watch(group1ActorRef)
+        group1ActorRef ! PoisonPill
+        probe.expectTerminated(group1ActorRef)
 
-//    probe.watch(toShutdown)
-//    toShutdown ! PoisonPill
-//    probe.expectTerminated(toShutdown)
+        probe.awaitAssert {
+          managerActor.tell(RequestGroupList(requestId = 1), probe.ref)
+          probe.expectMsg(ReplyGroupList(requestId = 1, Set("group2")))
+        }
+      }
+    }
 
-//    probe.awaitAssert {
-//      managerActor.tell(RequestGroupList(requestId = 1), probe.ref)
-//      probe.expectMsg(ReplyGroupList(requestId = 1, Set("group2")))
-//    }
   }
 
 
