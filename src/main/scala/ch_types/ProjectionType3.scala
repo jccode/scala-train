bd62a812977470db60e013edb75490afaf48b95b
junchangchen <junchangchen@sf-express.com>
2018-03-14 16:33:54 +0800

    type test 3

--- src/main/scala/ch_types/ProjectionType3.scala
+++ src/main/scala/ch_types/ProjectionType3.scala
@@ -1,12 +1,19 @@
 package ch_types
 
+import utils.CommonOpts
+
 /**
   * ProjectionType3
   *
   * @author 01372461
   */
 object ProjectionType3 extends App {
-
+  private val foo = new Foo
+  private val holder = new FooTypeHolder
+  private val client = new Client(holder)
+//  client.showElementType(foo) // TODO why compile error this line?
+  println("--------")
+  println(CommonOpts.typeOf(client.holder))
 }
 
 class Foo
@@ -15,5 +22,12 @@ trait TypeHolder[T] {
   type ElementType = T
 }
 
-class FooTypeHolder[Foo]
+class FooTypeHolder extends TypeHolder[Foo]
 
+class Client[T <: TypeHolder[_]](val holder: T) {
+
+  def showElementType(t: T#ElementType): Unit = {
+    println("show element type")
+    println(CommonOpts.typeOf(t))
+  }
+}
