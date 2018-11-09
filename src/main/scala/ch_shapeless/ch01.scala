9ba65ef4723a15807cf29c0f52581c8ba2d8510a
jccode <junchangchen@sf-express.com>
2018-11-09 16:31:40 +0800

    shapeless

--- src/main/scala/ch_shapeless/ch01.scala
+++ src/main/scala/ch_shapeless/ch01.scala
@@ -9,14 +9,12 @@ import shapeless._
   */
 object ch01 extends App {
 
-  case class Employee(name: String, number: Int, manager: Boolean)
-  case class IceCream(name: String, numCherries: Int, inCone: Boolean)
-
   val genericEmployee = Generic[Employee]
   val genericIceCream = Generic[IceCream]
 
   def genericCsv(gen: String :: Int :: Boolean :: HNil): List[String] =
-    List(gen(0), gen(1).toString, gen(2).toString)
+    //List(gen(0), gen(1).toString, gen(2).toString)
+    List(gen.head, gen.tail.head.toString, gen.tail.tail.head.toString)
 
   println(genericCsv(genericEmployee.to(Employee("Dave", 123, false))))
   println(genericCsv(genericIceCream.to(IceCream("Sundae", 1, false))))
---
+++ src/main/scala/ch_shapeless/ch03.scala
@@ -0,0 +1,75 @@
+package ch_shapeless
+
+import shapeless.{:+:, ::, CNil, Coproduct, Generic, HList, HNil, Inl, Inr}
+
+/**
+  * ch03
+  *
+  * @author 01372461
+  */
+object ch03 {
+
+  trait CsvEncoder[A] {
+    def encode(value: A): List[String]
+  }
+
+  object CsvEncoder {
+    // "summoner" method
+    def apply[A](implicit encoder: CsvEncoder[A]) = encoder
+
+    // "constructor" method
+    def instance[A](func: A => List[String]) = new CsvEncoder[A] {
+      override def encode(value: A): List[String] = func(value)
+    }
+
+    // Global visible type class instance
+    implicit val stringEncoder: CsvEncoder[String] = instance[String](x => List(x))
+    implicit val intEncoder: CsvEncoder[Int] = instance[Int](x => List(x.toString))
+    implicit val booleanEncoder: CsvEncoder[Boolean] = instance[Boolean](x => List(if (x) "yes" else "no"))
+    implicit val doubleEncoder: CsvEncoder[Double] = instance[Double](x => List(x.toString))
+
+    // HList encoder
+    implicit val hnilEncoder: CsvEncoder[HNil] = instance[HNil](x => Nil)
+    implicit def hlistEncoder[H, T <: HList](implicit hEncoder: CsvEncoder[H], tEncoder: CsvEncoder[T]): CsvEncoder[H :: T] = instance[H :: T] {
+      case h :: t => hEncoder.encode(h) ++ tEncoder.encode(t)
+    }
+
+    // Coproduct encoder
+    implicit val cnilEncoder: CsvEncoder[CNil] = instance[CNil](x => throw new Exception("Inconceivable!"))
+    implicit def coproductEncoder[L, R <: Coproduct](implicit lEncoder: CsvEncoder[L], rEncoder: CsvEncoder[R]): CsvEncoder[L :+: R] = instance[L :+: R] {
+      case Inl(l) => lEncoder.encode(l)
+      case Inr(r) => rEncoder.encode(r)
+    }
+
+    // Given a type A and HList type R, an implicit Generic to map A to R, and a CsvEncoder for R, create a CsvEncoder for A
+    implicit def genericEncoder[A, R](implicit gen: Generic.Aux[A, R], enc: CsvEncoder[R]): CsvEncoder[A] = instance[A](x => enc.encode(gen.to(x)))
+  }
+
+  def writeCsv[A](values: List[A])(implicit enc: CsvEncoder[A]): String = values.map(enc.encode(_).mkString(",")).mkString("\n")
+}
+
+object CsvEncoderApp extends App {
+  import ch03._
+
+  def hlistTest(): Unit = {
+    //val reprEncoder: CsvEncoder[String :: Int :: Boolean :: HNil] = implicitly
+    val reprEncoder: CsvEncoder[String :: Int :: Boolean :: HNil] = CsvEncoder[String :: Int :: Boolean :: HNil]
+    println(reprEncoder.encode("Hello" :: 114 :: true :: HNil))
+  }
+
+  def productTest(): Unit = {
+    val employeeEncoder = CsvEncoder[Employee]
+    println(employeeEncoder.encode(Employee("Hello", 20, true)))
+
+    val iceCreamEncoder = CsvEncoder[IceCream]
+    println(iceCreamEncoder.encode(IceCream("Hello", 20, false)))
+  }
+
+  def coproductTest(): Unit = {
+    val shapeEncoder = CsvEncoder[Shape]
+    println(writeCsv(List[Shape](Rectangle(3.0, 4.0), Circle(1.0))))
+  }
+
+  coproductTest()
+}
+
